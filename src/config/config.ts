0b442fd454d65b47005ffc790dd9bb19392ef3f6
eeshani23srivastava-cmyk <eeshani23srivastava@gmail.com>
2026-02-03 22:25:40 +0530

    Correct name spelling in siteConfig

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ export const getAsset = (path: string) => {
 
 const siteConfig = {
   personal: {
-    name: 'EEshani Srivaastava',
+    name: 'Eeshani Srivaastava',
     title: 'Undergraduate Student| Aspiring Data Scientist',
     image: getAsset('images/profile.png'), // Customize or replace with your profile image
     description:
